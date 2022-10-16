9c214277db7789337cede9a18b175b452f1a16c7
Brady Ginsburg <bradyginsburg@gmail.com>
2022-10-16 17:36:17 -0500

    change setup so canvas.js can run at the start

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-function main() {
-    
+function start() {
+
 }
+
+function loop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    requestAnimationFrame(animate);
+    main();
+}
+
+start();
+loop();

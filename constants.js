e5b78cfcd2449d07f6bebc8a2ec284a0644b6a86
muzan-0 <80413333+muzan-0@users.noreply.github.com>
2022-10-17 20:51:55 -0500

    did epic stuff (made window scale)

--- constants.js
+++ constants.js
@@ -1,10 +1,10 @@
-const SCROLL_SPEED = 1;
+const SCROLL_SPEED = 10;
 const OBSTICLE_SIZE = 75;
 const GROUND_HEIGHT = 100;
 const PLAYER_SIZE = 75;
 const PLAYER_X = 75;
 const GRAVITY = -1; //pixels per frame
 const JUMP_VELOCITY = 20;
-const GAME_WIDTH = 1200;
-const GAME_HEIGHT = 675
+const GAME_HEIGHT = window.innerHeight;
+const GAME_WIDTH = GAME_HEIGHT * 16/9
 const FPS = 60
